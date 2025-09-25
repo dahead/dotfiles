78b23e2c9cbaeecd5888946c47a07b5a5f3f96bd
dahead <dahead@mailbox.org>
2025-09-25 10:42:16 +0200

    Add files via upload

--- .config/hypr/scripts/random_wallpaper.sh
+++ .config/hypr/scripts/random_wallpaper.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
-
 WALLPAPER_DIR="$HOME/Wallpaper/"
-CURRENT_WALL=$(hyprctl hyprpaper listloaded)
 
-# Get a random wallpaper that is not the current one
-WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
+# Get a random wallpaper that's not currently loaded
+WALLPAPER=$(find "$WALLPAPER_DIR" -type f | while IFS= read -r file; do
+    filename=$(basename "$file")
+    if ! hyprctl hyprpaper listloaded | grep -q "$filename"; then
+        echo "$file"
+    fi
+done | shuf -n 1)
 
-# Apply the selected wallpaper
-hyprctl hyprpaper reload ,"$WALLPAPER"
+# Get the name of the focused monitor with hyprctl
+FOCUSED_MONITOR=$(hyprctl monitors -j | jq -r '.[] | select(.focused) | .name')
+
+# Check if we found a wallpaper
+if [[ -n "$WALLPAPER" ]]; then
+    # hyprctl hyprpaper preload $WALLPAPER
+    # hyprctl hyprpaper wallpaper ",$WALLPAPER"
+    hyprctl hyprpaper reload "$FOCUSED_MONITOR","$WALLPAPER"
+else
+    echo "No available wallpapers found"
+fi
