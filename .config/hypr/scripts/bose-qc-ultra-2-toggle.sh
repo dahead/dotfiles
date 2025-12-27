e33be172148980cef1105f5197bbf778eec01957
dahead <dahead@mailbox.org>
2025-12-27 16:33:27 +0100

    Add files via upload

--- .config/hypr/scripts/bose-qc-ultra-2-toggle.sh
+++ .config/hypr/scripts/bose-qc-ultra-2-toggle.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
-
-STATUS_FILE="/tmp/bose-soundlink-max_script_status.txt"
 MAC_ADDRESS="E4:58:BC:BF:AE:FE"
 
-# Überprüfen, ob die Datei existiert und den aktuellen Status lesen
-if [[ -f "$STATUS_FILE" ]]; then
-    STATUS=$(cat "$STATUS_FILE")
-else
-    STATUS=0
-fi
-
-if [[ "$STATUS" -eq 0 ]]; then
-    notify-send "BOSE QuietComfort Ultra 2nd Gen. connected"
-    bluetoothctl connect $MAC_ADDRESS
-    echo 1 > "$STATUS_FILE"
-else
+# Check if device is currently connected
+if bluetoothctl info "$MAC_ADDRESS" | grep -q "Connected: yes"; then
     notify-send "BOSE QuietComfort Ultra 2nd Gen. disconnected"
-    bluetoothctl disconnect $MAC_ADDRESS
-    echo 0 > "$STATUS_FILE"
+    bluetoothctl disconnect "$MAC_ADDRESS"
+else
+    notify-send "BOSE QuietComfort Ultra 2nd Gen. connected"
+    bluetoothctl connect "$MAC_ADDRESS"
 fi
