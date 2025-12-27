72feb6bc54546b4850e7762106163542a80bf62d
dahead <dahead@mailbox.org>
2025-12-27 16:36:39 +0100

    Add files via upload

--- .config/hypr/scripts/Wobkey80-pair.sh
+++ .config/hypr/scripts/Wobkey80-pair.sh
@@ -1,1 +1,13 @@
-bluetoothctl connect E4:17:D8:97:19:98
+#!/bin/bash
+MAC_ADDRESS="E4:17:D8:97:19:98"
+
+# Check if device is currently connected
+if bluetoothctl info "$MAC_ADDRESS" | grep -q "Connected: yes"; then
+    notify-send "Wobkey 80 disconnected"
+    bluetoothctl disconnect "$MAC_ADDRESS"
+else
+    notify-send "Wobkey 80 connected"
+    bluetoothctl connect "$MAC_ADDRESS"
+fi
+
+bluetoothctl connect 
